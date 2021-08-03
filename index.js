d9cce07499215586c1bd3e9a65793d1456f30019
Titus Wormer <tituswormer@gmail.com>
2021-08-03 17:58:38 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+/**
+ * @typedef {import('mdast').Root} Root
+ */
+
 import {normalizeHeadings} from 'mdast-normalize-headings'
 
+/**
+ * Plugin to make sure that there is only one top-level heading in a document
+ * by adjusting heading depths accordingly.
+ *
+ * @type {import('unified').Plugin<void[], Root>}
+ */
 export default function remarkNormalizeHeadings() {
   return normalizeHeadings
 }
